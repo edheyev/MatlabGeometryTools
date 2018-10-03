1fd0565eb0711c649c2f262ed210c5d057afa78c
EdMac <edheyev@gmail.com>
2018-10-03 02:19:33 +0100

    trying to get internal angle calc to work

--- GetCornerAngles.m
+++ GetCornerAngles.m
@@ -1,54 +1,23 @@
-function [cornerAngles] = GetCornerAngles(xs, ys, lengths, order)
+function [cornerAngles] = GetCornerAngles(xs, ys, sideLengths, order)
 cornerAngles = [];
-% for i  = 1:length(order)-1
-%     cornerAngles = [cornerAngles, ];    
-% end
-
-A = 1;
-B = 2;
-C = 3;
-D = 4;
-hold on
-plot([xs(order(B)) xs(order(D))],[ys(order(B)) ys(order(D))], '*')
-for i = 1:length(lengths)
-
-    da = lengths(order(A))
-    db = pdist2([xs(order(B)) ys(order(B))],[xs(order(D)) ys(order(D))], 'euclidean')
-    
-    plot([xs(order(B)) xs(order(D))],[ys(order(B)) ys(order(D))])
-    
-    dc = lengths(order(C))
-    
-    if(A >= length(lengths))
-        A = 1;
-    else
-        A = A + 1;
-    end
-    if (B >= length(lengths))
-        B = 1;
-     else
-        B = B + 1;
-    end
-    
-    if (C >= length(lengths))
-        C = 1;
-    else
-        C = C + 1;
-    end
-     if (D >= length(lengths))
-        D = 1;
-    else
-        D = D + 1;
-    end
+orderlong = [unique(order),unique(order),unique(order)];
+for i = 1: length(sideLengths)
+    x1 = xs(orderlong(i + 1))
+    y1 = ys(orderlong(i + 1))
+    xc = xs(orderlong(i))
+    yc = ys(orderlong(i))
+    x2 = xs(orderlong(i + 3))
+    y2 = ys(orderlong(i + 3))
     
     
-    %p2 = centre point distances are P2-P1 = A, P1-P3 = B and P3-P2 = C:
-
-    (dc^2) +  (da^2) - (db^2)/(2*dc*da)
-    %Angle = radtodeg(acos(((da^2)+(dc^2)-(db^2))/(2*da*dc)))
-    angle = radtodeg(real(acos(((dc^2) +  (da^2) - (db^2))/(2*dc*da))))
-    cornerAngles = [cornerAngles, angle];
+    lenA = pdist2([xc,yc],[x1,y1],'euclidean');
+    lenB =pdist2([xc,yc],[x2,y2],'euclidean');
+    lenC =pdist2([x1,y1],[x2,y2],'euclidean');
     
+    angleC = rad2deg(real(acos(((lenA^2) +  (lenB^2) - (lenC^2))/(2*lenA*lenB))))
+    
+    cornerAngles = [cornerAngles, angleC];
 end
-%cornerAngles = radtodeg(cornerAngles);
+
+
 end
